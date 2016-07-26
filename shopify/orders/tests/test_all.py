ba74b1650e62114d5102b9d73c0e7feb8d91bde1
Mark <ziplokk.mark.sanders@gmail.com>
2016-07-26 15:41:53 -0500

    Added test_all.py to tests

---
+++ shopify/orders/tests/test_all.py
@@ -0,0 +1,14 @@
+import unittest
+
+from test_address import TestAddress
+from test_clientDetail import TestClientDetail
+from test_customer import TestCustomer
+from test_lineItem import TestLineItem
+from test_order import TestOrder
+from test_paymentDetail import TestPaymentDetail
+from test_shippingLine import TestShippingLine
+from test_taxLine import TestTaxLine
+
+
+if __name__ == '__main__':
+    unittest.main()
