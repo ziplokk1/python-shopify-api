4dba1b100740e4e42352a6c44d8badfacbb7e2bc
Mark <ziplokk.mark.sanders@gmail.com>
2016-09-08 12:34:24 -0500

    Moved count to products.init to simplify imports

--- shopify/products/__init__.py
+++ shopify/products/__init__.py
@@ -1,2 +1,12 @@
 from objects import *
 from api_wrapper import ProductsApiWrapper
+
+
+def count(api_key, api_password, store_name):
+    import json
+    import requests
+
+    url = 'https://{}:{}@{}.myshopify.com/admin/products/count.json'.format(api_key, api_password, store_name)
+    response = requests.get(url)
+    data = json.loads(response.content)
+    return int(data['count'])
